e8107f8905592ba58afd8414f3ea4b93c584a392
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-09-08 15:28:45 -0300

    :hammer_and_wrench: build: Add Jest configuration

---
+++ horario-atendimento/jest.config.js
@@ -0,0 +1,5 @@
+export const testEnvironment = 'node';
+export const collectCoverage = true;
+export const coverageDirectory = 'coverage';
+export const coverageProvider = 'v8';
+export const testPathIgnorePatterns = ['/node_modules/'];
