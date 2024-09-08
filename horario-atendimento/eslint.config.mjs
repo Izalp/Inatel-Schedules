5808849d74cf66cea5feeb380ad318fd45729f8c
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-09-08 16:58:15 -0300

    :wrench: chore: Update ESLint configuration

--- horario-atendimento/eslint.config.mjs
+++ horario-atendimento/eslint.config.mjs
@@ -1,8 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.jest,
+    },
+  },
+  extends: [
+    pluginJs.configs.recommended,
+    'plugin:jest/recommended',
+  ],
+  plugins: [
+    'jest',
+  ]
+};
